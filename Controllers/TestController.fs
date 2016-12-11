98f0d08fee40e19d4a8601a0d961dc3653b6b045
Jip Stavenuiter <j.stavenuiter@students.uu.nl>
2016-12-11 21:03:18 +0100

    Initial commit

---
+++ Controllers/TestController.fs
@@ -0,0 +1,11 @@
+namespace Controllers
+
+open Microsoft.AspNetCore.Mvc
+open Microsoft.AspNetCore.Routing
+
+type TestController() = 
+    inherit Controller()
+
+    [<HttpGet>]
+    member this.Test() =
+        "TestSuccesfull!"
---
+++ Program.fs
@@ -0,0 +1,35 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open Microsoft.AspNetCore.Hosting
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Http
+open Microsoft.AspNetCore.Routing
+open Microsoft.Extensions.Configuration
+open Microsoft.Extensions.DependencyInjection
+
+type Startup() =
+    member this.Configure(app:IApplicationBuilder) = 
+        let setStandardRoute (routes:IRouteBuilder) =  
+                routes.MapRoute(
+                    name = "default",
+                    template = "{controller}/{action}/{id?}"
+                ) |> ignore
+        let app = app.UseMvc(fun r -> setStandardRoute r)
+        ()
+
+    member this.ConfigureServices(services:IServiceCollection) =
+        let services = services.AddMvc()
+        ()
+
+[<EntryPoint>]
+let main argv = 
+    let host = 
+        WebHostBuilder()
+            .UseKestrel()
+            .UseUrls("http://localhost:5000")
+            .UseIISIntegration()
+            .UseStartup<Startup>()
+            .Build()
+    host.Run()
+    0 // return an integer exit code
